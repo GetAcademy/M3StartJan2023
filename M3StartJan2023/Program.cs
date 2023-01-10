6ec0bc75ff73770de55953d7949851b9afc2efad
GET Academy <terje@getacademy.no>
2023-01-10 11:12:51 +0100

    Initial commit

---
+++ M3StartJan2023/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+namespace M3StartJan2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        //static void Main(string[] args)  //  function Main(args)
+        //{
+        //    var list = new ArrayList();
+
+        //    Console.WriteLine("Hello, World! ");
+        //    //Console.WriteLine(args[0]);
+        //    var n = 7;
+        //    DoubleAndWrite(n);
+
+
+        //    var a = 7 * 2 + 7;
+        //    var temp = a * 2 + 7;
+        //    var b = temp;
+        //    var c = temp*2;
+        //}
+
+        //static void DoubleAndWrite(int n)
+        //{
+        //    var value = Double(n);
+        //    Console.WriteLine(value);
+        //}
+
+
+        //static int Double(int a)
+        //{
+        //    return a * 2;
+        //}
+    }
+}
